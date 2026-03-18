b5c079e3605b05cb7fcd5732c1a813ce939dc37d
George Justus Morara <george@Georges-MacBook-Pro.local>
2026-03-18 21:43:25 +0000

    main.py edit

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from project.ingestion.extract_excel import run_excel_ingestion
 from project.transformations.clean_data import run_cleaning_pipeline
 from project.transformations.enrich_data import run_enrichment_pipeline
 from project.warehouse.load_to_db import load_to_database
-import os
 
-os.environ["BASE_DIR"] = os.getcwd()  # set base dir for relative paths in utils and project modules
+
 if __name__ == "__main__":
     run_excel_ingestion()
     run_cleaning_pipeline()
